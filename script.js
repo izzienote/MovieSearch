f0a755a51d9da55b6e444dbed2a4ad8af81d0c50
Jieun Lee <2ur.leeje@gmail.com>
2025-01-15 23:36:26 +0900

    ✨ update : 북마크 기능 추가

--- script.js
+++ script.js
@@ -18,8 +18,16 @@ const myVoteRate = document.querySelector("#myVoteRate");
 const myPoster = document.querySelector("#myPoster");
 // (0-4) 전역 변수 선언 _ fetch한 값을 담는 변수
 let movieData = [];
+// (0-5) 북마크 상수 선언
+const addBookmarkBtn = document.querySelector(".add-bookmark");
+const seeBookmarkBtn = document.querySelector(".checked-bookmark");
+let bookmarkedMovie = JSON.parse(localStorage.getItem('bookmarkItem')) || [];
+//JSON.parse : 결과값을 객체로 반환
 
-// (1)가져오는 fecth를 함수로 지정
+
+
+
+// (1)가져오는 fecth를 함수로 지정  _____ Before
 function fecthData() {
   fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=ko-KR&page=1&sort_by=popularity.desc', options)
     .then(res => res.json()) //응답을 JSON으로 변환
@@ -29,7 +37,8 @@ function fecthData() {
       movieData = res.results;  //전역 변수에 값 넣어줘야 find로 찾을때 값을 찾을 수 있음
       displayPosts(results);
     })
-}
+};
+
 
 // (2)포스팅 리스팅하는 fecth
 function displayPosts(results) {
@@ -48,7 +57,7 @@ function displayPosts(results) {
   <li class="movie-name">${movie.title}</li>
   <h6 class="movie-name hide">${movie.original_title}</h6>
   <li class="movie-rate">평점⭐️ : ${movie.vote_average}</li>
-  <li class="hide">${movie.id}</li>
+  <li class="">${movie.id}</li>
   `;
 
     movieContainer.appendChild(movieItem);
@@ -81,17 +90,21 @@ searchMovie.addEventListener("change", function () {
 movieContainer.addEventListener("click", function (event) {
   const movieCard = event.target.closest('.movie-card'); // 클릭한 요소가 movie-card인지 확인
 
+
   if (movieCard) { // 클릭한 요소가 movie-card일 경우
     const movieId = movieCard.getAttribute("data_id");
-    // alert(`영화 id : ${movieId}`);
+
+    // **여기서 addBookmarkBtn.onclick으로 수정**하여 이벤트 리스너를 한 번만 등록
+    addBookmarkBtn.onclick = function () {
+      addBookmark(movieId);
+    };
 
     // [방법1] 여기에서 무비 아이디랑 일치하는 아이 찾아서, 모달의 textContents를 바꿔줘라!
     // find로 찾은 값이 속해 있는 객체를 return함
     const selectedMovie = movieData.find(function (movieData) {
       return movieData.id.toString() === movieId;
     });
-    // 왜 리턴한 객체가 없지? -> movieData.id.toString으로 해결(데이터타입이 문자,숫자였던듯)
-    console.log(selectedMovie);
+
     // 선택된 무비의 정보에 맞춰서 modal 텍스트 바꿔주기
     if (selectedMovie) {
       myPoster.src = `https://image.tmdb.org/t/p/w400${selectedMovie.poster_path}`;
@@ -100,11 +113,8 @@ movieContainer.addEventListener("click", function (event) {
       myReleaseDate.textContent = `개봉일 : ${selectedMovie.release_date}`;
       myVoteRate.textContent = `평점⭐️ : ${selectedMovie.vote_average}`;
     }
+    toggleModal();
   }
-  toggleModal();
-
-  // [방법2] 여기에 새롭게 페칭하는 로직이 필요?.....ㅠㅠㅠㅠ
-  // fetch(`https://api.themoviedb.org/3/search/movie?include_adult=false&language=ko-KR&page=1`, options)
 });
 
 
@@ -120,10 +130,69 @@ modalCloseBtn.addEventListener("click", function () {
   toggleModal();
 });
 
-// (5-2) 모달 외 영역 클릭 시, 닫기
-// modal.addEventListener("click", function(e) {
-//   if(e.target === e.currentTarget){
-//     toggleModal();
-//   }
-// })
 
+// ------------------------------------------------------------
+// (6) 북마크 보기 버튼 클릭 시, getBookmark 함수 실행
+seeBookmarkBtn.addEventListener("click", function () {
+  showBookmark()
+});
+
+// ------------------------------------------------------------
+
+// (7-1) 북마크 add 함수 ()
+function addBookmark(movieId) {
+  movieId = Number(movieId);
+
+  if (!bookmarkedMovie.includes(movieId)) {
+    bookmarkedMovie.push(movieId);
+    localStorage.setItem('bookmarkItem', JSON.stringify(bookmarkedMovie));
+    alert("북마크 추가 성공!");
+  }
+  else {
+    alert("이미 북마크에 추가된 영화입니다. 다른 영화를 선택해주세요");
+  }
+};
+// ------------------------------------------------------------
+
+// (7-2) 북마크 get 함수 () ___ 값이 잘 들어왔나 확인용
+function getBookmark() {
+  console.log(localStorage.getItem('bookmarkItem'));
+  alert("북마크 보기 버튼이 클릭되었습니다");
+};
+// ------------------------------------------------------------
+
+// (7-3) 북마크 show 함수 (북마크된 영화만 다시 리스팅)
+function showBookmark() {
+  // 북마크된 영화들ID에 대한 상수 선언 및 movieData(페치한 데이터)에서 찾기
+  const bookmarkedMovies = movieData.filter(function (movie) {
+    return bookmarkedMovie.includes(movie.id);
+  });
+
+  // 북마크된 영화 보여주기
+  displayBookmarkedPosts(bookmarkedMovies);
+}
+// ------------------------------------------------------------
+
+// (8) 북마크된 포스트 보기
+function displayBookmarkedPosts(movieId) {
+  // 기존 콘텐츠 지우기****초기화
+  movieContainer.innerHTML = '';
+
+  movieId.forEach((movie) => {
+
+    const movieItem = document.createElement('li');
+    movieItem.className = 'movie-card no_dot';
+    movieItem.setAttribute('data_id', movie.id);  // 영화 id를 data-id로 설정
+    movieItem.innerHTML = `
+  <li>
+    <img class="movie-image" src="https://image.tmdb.org/t/p/w200${movie.poster_path}" alt="이미지를 찾을 수 없습니다" height="280px">
+  </li>
+  <li class="movie-name">${movie.title}</li>
+  <h6 class="movie-name hide">${movie.original_title}</h6>
+  <li class="movie-rate">평점⭐️ : ${movie.vote_average}</li>
+  <li class="">${movie.id}</li>
+  `;
+
+    movieContainer.appendChild(movieItem);
+  });
+}
